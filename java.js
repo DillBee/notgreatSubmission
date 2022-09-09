ad5eca6927e8ab1c8837e80b4b37ac5f9ed016c7
DillBee <109165003+DillBee@users.noreply.github.com>
2022-09-09 10:10:25 +0100

    code

---
+++ java.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+
+function inNew() {
+    fs.readFile('numbers.txt', 'utf8', (err, data) => {
+        var numarray = data.split(',')
+        let inorder = false
+
+        numarray = removeDUPLICATS(numarray)
+
+        while(!inorder){
+            numarray = bubblesort(numarray)
+            inorder = is_it_in_order(numarray)
+            console.log(numarray)
+        }
+        
+        console.log("The numbers are in order.", numarray)
+      });
+}
+
+function bubblesort(array) {
+    for (var i = 0; i < array.length; i++) {
+        var j = i+1
+        if (parseInt(array[i]) > parseInt(array[j])){
+            var valueone = array[i]
+            var valuetwo = array[j]
+            array[i] = valuetwo
+            array[j] = valueone
+        }
+    }
+    console.log("Sorted ", array)
+    return array
+}
+
+
+function removeDUPLICATS(array) {
+    let newarray = []
+    for (var i = 0; i < array.length; i++) {
+        if (!newarray.includes(array[i])){
+            newarray.push(array[i])
+        }
+    }
+    console.log("Dupes removed")
+    console.log(newarray)
+    return newarray
+}
+
+
+function is_it_in_order(array) {
+    var comparitor = array[0]
+    for (var i = 1; i < array.length; i++) {
+        console.log("Is ", array[i], " less then ", comparitor)
+        if (parseInt(array[i]) < parseInt(comparitor)){
+            return false
+            }
+        comparitor = array[i]
+    }
+    return true
+}
+
+//copied from stack overflow :D
+function Shufflearray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+
+inNew()
